1bd7785851cb7325958717d8802ee64513ad450d
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-29 15:34:26 +0200

    Add: importing vue-router on front.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,9 +1,16 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 window.Vue = require('vue');
+
+Vue.use(VueRouter)
 
 // Importing App component
 import App from './views/App';
+import router from './router';
 
 const app = new Vue({
     el: '#root',
-    render: h => h(App) // Show App when Vue starts
+    render: h => h(App), // Show App when Vue starts
+    router 
 });
