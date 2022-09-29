999f0118a09e87ab6f34bb3cc3c57d1a901a3e20
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-29 17:21:14 +0200

    Style: add style to contacts page

--- resources/js/pages/ContactsPage.vue
+++ resources/js/pages/ContactsPage.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        Pagina Contacts
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <div class="text-center text-white bg-dark rounded p-2 mt-5">
+                    <h1>This is the Contacts Page</h1>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
