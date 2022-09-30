a9dd2e8c0e349dc898592a9e25568287c777ff65
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-30 15:07:07 +0200

    Test: TagsIndexPage is connected

--- public/js/front.js
+++ public/js/front.js
@@ -2558,7 +2558,7 @@ var render = function render() {
   var _vm = this,
       _c = _vm._self._c;
 
-  return _c("div");
+  return _c("div", [_vm._v("\n    Tags\n")]);
 };
 
 var staticRenderFns = [];
